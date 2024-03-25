6001d4558c945416b3b0ec9d177ab40fc04c913e
kimkimgungunwoo <gunwoo7179@gmail.com>
2024-03-25 16:34:24 +0900

    r

--- 프로그래머스/Lv1_가장많이받은선물.py
+++ 프로그래머스/Lv1_가장많이받은선물.py
@@ -1,22 +1,3 @@
-'''
-친구가 서로 주고받은 횟수->그래프 이용/탑색들어가서 비교
-선물지수->그래프 탐색필요할듯?
-
-그래프에서 본인꺼에는 선물지수 체크해두자
-
-
-
-friends->친구목록-->friends*friends만큼의 2중배열
-gifts분석해서 하면 될듯?
-
-
-주어지는게 이름-->이름을 번호에 매핑?(딕셔너리)
-아니면 이름만 가지고 하기?
-
-가장많은 선물 받은사람의 선물 개수 
-'''
-
-
 def solution(friends, gifts):
     friendsdict={friends[x]:x for x in range(len(friends))}
     giftsfield=[[0 for x in range(len(friends))] for k in range(len(friends))]
@@ -25,7 +6,7 @@ def solution(friends, gifts):
         b=gifts[i].split()[1]
         giftsfield[friendsdict[a]][friendsdict[b]]+=1 #선물 준 사람껑 체크
         giftsfield[friendsdict[a]][friendsdict[a]]+=1 #준사람 선물 지수 증가
-        giftsfield[friendsdict[b]][friendsdict[b]]=-1 #받은사람 선물 지수 감
+        giftsfield[friendsdict[b]][friendsdict[b]]-=1 #받은사람 선물 지수 감
     maxanswer=0
     for i in range(len(giftsfield)):
         answer=0
@@ -41,4 +22,3 @@ def solution(friends, gifts):
            
     return maxanswer
 
-print(solution(["joy", "brad", "alessandro", "conan", "david"],["alessandro brad", "alessandro joy", "alessandro conan", "david alessandro", "alessandro david"],))
