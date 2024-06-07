ef0002fd37e3fd43804e31ed868bf3b43db00fb1
kimkimgungunwoo <gunwoo7179@gmail.com>
2024-06-08 00:30:45 +0900

    g3_2242_줄세우기

---
+++ 백준/골드/G3_2252_줄세우기.py
@@ -0,0 +1,37 @@
+'''
+반복문 설정이 애매하넹
+'''
+
+import sys
+input=sys.stdin.readline
+
+N,M=map(int,input().split())
+std_lst=[0]*(N+1)
+std_lst[0]=-1
+field=[[] for x in range(N+1)]
+AnswerList=[]
+zeroque=[]
+for i in range(M):
+    a,b=map(int,input().split())
+    field[a].append(b)
+    std_lst[b]+=1
+
+for i in range(N+1):
+    if std_lst[i]==0:
+        zeroque.append(i)
+        AnswerList.append(i)
+while zeroque:
+    A=zeroque.pop(0)
+    for i in field[A]:
+        if std_lst[i]>0:
+            std_lst[i]-=1
+            if std_lst[i]==0:
+                zeroque.append(i)
+                AnswerList.append(i)
+    
+for i in range(len(AnswerList)):
+    print(AnswerList[i],end=" ")
+        
+        
+    
+    
